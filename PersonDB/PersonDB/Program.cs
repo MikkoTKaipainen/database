39709c20a806e1659122b54027341f48c0c43e6f
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2018-03-22 12:01:10 +0200

    new project with database

---
+++ PersonDB/PersonDB/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PersonDB.model;
+using PersonDB.Repositories;
+
+namespace PersonDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Database CRUD operations");
+            //Person person = new Person("Mikko",25);
+            //PersonRepository.Create(person);
+
+            var persons = PersonRepository.Get();
+            foreach (var p in persons)
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+           
+        }
+    }
+}
